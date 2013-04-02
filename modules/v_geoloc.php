10480bab142a5bcbe8f631aac19c3b4206bdb950
Nettelekom <j.mociak@nettelekom.pl>
2013-04-02 11:37:15 +0200

    Poprawka błędu przy wyborze lokalizacji klienta

--- modules/v_geoloc.php
+++ modules/v_geoloc.php
@@ -1,13 +1,13 @@
 <?php
 if($_GET['type'] == 'p')
 {
-	$res = $voip->list_pow($_GET['id']);
+	$res = $voip->wsdl->list_pow($_GET['id']);
 	if($res) foreach($res as $key => $val)
 	echo "obj.options[obj.options.length] = new Option('$val','$key');";
 }
 elseif($_GET['type'] == 'm')
 {
-	$res = $voip->list_mia($_GET['id']);
+	$res = $voip->wsdl->list_mia($_GET['id']);
 	if($res) foreach($res as $key => $val)
 	echo "obj.options[obj.options.length] = new Option('$val','$key');";
 }
